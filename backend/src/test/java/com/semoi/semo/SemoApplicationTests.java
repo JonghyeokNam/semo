2712d29342c75d006b559966747e7ca471f92b9a
brightCha <ckguscjf03@gmail.com>
2024-12-10 09:10:48 +0900

    init: backend init

---
+++ backend/src/test/java/com/semoi/semo/SemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semoi.semo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
