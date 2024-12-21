9811d9626f5ba3bec70ba7933c9616b95f392e5a
brightCha <ckguscjf03@gmail.com>
2024-12-21 16:18:33 +0900

    feat #EM-7: OAuth2 관련 코드 작성

---
+++ backend/src/main/java/com/semoi/semo/oauth2/dto/OAuth2Response.java
@@ -0,0 +1,11 @@
+package com.semoi.semo.oauth2.dto;
+
+import java.util.Map;
+
+public interface OAuth2Response {
+
+    String getProvider(); //제공자 (Ex. naver, google, ...)
+    String getEmail(); //이메일
+    String getName(); //사용자 실명 (설정한 이름)
+    Map<String, Object> getAttribute();
+}
