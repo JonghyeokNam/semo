2f075d082689fad4791b993152aed53d966f5491
이유진 <jenny7732@naver.com>
2024-12-24 14:22:30 +0900

    feat #EM-39: 게시글 상세 페이지 ui 작성

---
+++ frontend/src/pages/board/detail/back.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import * as S from "./style";
+
+// 뒤로가기 버튼
+export const Back = () => {
+  return (
+    <S.BackButton>
+      <S.BackIcon />
+    </S.BackButton>
+  );
+};
