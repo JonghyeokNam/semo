b61bd3018ac579a8b034fa9eafa9a5c767c717a0
이유진 <jenny7732@naver.com>
2024-12-26 15:48:40 +0900

    feat #EM-43: 지원리스트 페이지 작성글 ui

--- frontend/src/pages/applylist/index.jsx
+++ frontend/src/pages/applylist/index.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import * as S from "./style";
+import Container from "../mypage/container";
+import BoardListApplyList from "../../components/ui/board/boardListApplyList";
+import ApplyComponent from "./applyComponent";
 
+const Index = () => {
 
-const index = () => {
+  useEffect(() => {
+    // 컴포넌트가 마운트될 때 화면 상단으로 스크롤
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []); // 빈 배열을 넣어서 첫 렌더링 시 한 번만 실행되도록 설정
+  
   
   return (
-    <div>지원 리스트 페이지</div>
+    <S.applyWrapper>
+      <S.MainTitle>지원 리스트</S.MainTitle>
+      <Container title="작성글" type="작성">
+        <BoardListApplyList/>
+      </Container>
+      <Container title="지원자" type="지원" >
+        <ApplyComponent/>
+        <ApplyComponent/>
+      </Container>
+    </S.applyWrapper>
   );
 };
 
-export default index;
+export default Index;
