f75e838344ca479c12eaf4f0a8bdbb4675731913
brightCha <ckguscjf03@gmail.com>
2024-12-27 13:32:03 +0900

    feat #EM-16: campus controller&Service create

--- backend/src/main/java/com/semoi/semo/Campus/service/CampusService.java
+++ backend/src/main/java/com/semoi/semo/Campus/service/CampusService.java
@@ -1,8 +1,16 @@
 package com.semoi.semo.Campus.service;
 
 import com.semoi.semo.Campus.domain.Campus;
+import com.semoi.semo.Campus.dto.CampusActScoreResponseDto;
+import com.semoi.semo.Campus.dto.CampusRecScoreResponseDto;
 import com.semoi.semo.Campus.enums.CampusName;
+
+import java.util.List;
 
 public interface CampusService {
     Campus getCampusOrElseThrow(CampusName campusName);
+    List<CampusRecScoreResponseDto> getCampusRecRankingByYear(int year);
+    List<CampusActScoreResponseDto> getCampusActRankingByYear(int year);
+
+    void calculateAndSaveCampusYearlyScores();
 }
