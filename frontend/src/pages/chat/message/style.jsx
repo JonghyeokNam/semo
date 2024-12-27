a37306d991f52d4195ff4423d2c43824b1f110bf
이유진 <jenny7732@naver.com>
2024-12-27 15:56:18 +0900

    feat #EM-44: chat 메세지 ui 작성

---
+++ frontend/src/pages/chat/message/style.jsx
@@ -0,0 +1,116 @@
+import styled from "styled-components";
+
+export const ChatRoomContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`;
+
+export const ChatTitle = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+
+export const MessagesContainer = styled.div`
+  box-shadow: 0px 4px 48px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  background-color: #ffffff;
+  width: ${(props) => (props.$isMobile ? "300px" : "400px")}; 
+  height: ${(props) => (props.$isMobile ? "400px" : "477px")}; 
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto; 
+  padding: 20px;
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: ${(props) => (props.isUser ? "row-reverse" : "row")}; /* 사용자 메시지는 오른쪽으로, 상대방은 왼쪽으로 배치 */
+  align-items: flex-start;
+  margin-bottom: 10px;
+  gap: 10px;
+`;
+
+export const UserName = styled.div`
+  display: flex;
+  flex-direction: ${(props) => (props.isUser ? "row-reverse" : "row")};
+  font-weight: bold;
+  font-size: 14px;
+  margin-bottom: 5px; 
+`;
+
+export const Content = styled.div`
+  font-size: 14px;
+  background-color: #F1F1F1;
+  border-radius: 15px;
+  padding: 10px 15px;
+  word-wrap: break-word;
+  display: inline-block; 
+  position: relative;
+  z-index: 0;
+`;
+
+export const Message = styled.div``;
+
+export const Time = styled.div`
+  font-size: 12px;
+  color: #777;
+  margin-top: 5px;
+  align-self: flex-end; 
+`;
+
+// 왼쪽 말풍선 꼬리
+export const Tail = styled.div`
+  border-top: 15px solid #f1f1f1;
+  border-left: 15px solid transparent;
+  content: "";
+  position: absolute;
+  left: -12px;  
+  top: 12px;
+  z-index: 10; 
+`;
+
+// 오른쪽 말풍선 꼬리 (사용자 메시지)
+export const UserTail = styled.div`
+  border-top: 15px solid #f1f1f1;
+  border-right: 15px solid transparent;
+  content: "";
+  position: absolute;
+  top: 10px;
+  right: -12px;  
+  z-index: 10;  
+`;
+
+export const MessageInputContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  border-top: 1px solid #ddd;
+  position: absolute;
+  bottom: -73px;
+  width: 100%; 
+  z-index: 10; 
+`;
+
+export const MessageInput = styled.textarea`
+  width: 80%;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  resize: none;
+`;
+
+export const SendButton = styled.button`
+  padding: 10px 15px;
+  background-color: var(--green);
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
