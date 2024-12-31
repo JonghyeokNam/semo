e6c72a8f4febdc347e05a3dcd5437f98d0cf08c5
이유진 <jenny7732@naver.com>
2024-12-31 10:33:24 +0900

    feat #EM-93: 랭킹 데이터 가져오기

---
+++ frontend/src/pages/home/ActRank.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+import * as S from "./style";
+import { useActRankStore } from '../../store/useRankStore';
+
+const ActRank = () => {
+  const { ranks, fetchRanks, loading, error } = useActRankStore();
+
+  useEffect(() => {
+    fetchRanks(); // 데이터 가져오기
+  }, [fetchRanks]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <S.RankContainer>
+      <S.RankTable>
+        <thead>
+          <tr>
+            <S.TableHeader>순위</S.TableHeader>
+            <S.TableHeader>캠퍼스</S.TableHeader>
+            <S.TableHeader>점수</S.TableHeader>
+          </tr>
+        </thead>
+        <tbody>
+          {ranks.map((item, index) => (
+            <tr key={index}>
+              {/* 순위를 정적으로 매핑 (인덱스 + 1) */}
+              <S.TableCell $rank={index + 1}>{index + 1}</S.TableCell>
+              <S.TableCell $rank={index + 1}>{item.campusName} 캠퍼스</S.TableCell>
+              <S.TableCell $rank={index + 1}>{item.score}</S.TableCell>
+            </tr>
+          ))}
+        </tbody>
+      </S.RankTable>
+    </S.RankContainer>
+  );
+};
+
+export default ActRank;
