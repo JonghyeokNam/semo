b1c8f955c5b1dec447b47aa30f982746edf84bce
이유진 <jenny7732@naver.com>
2025-01-02 18:55:01 +0900

    feat #EM-101: nav 사용자 이름 가져오기 테스트

--- frontend/src/layouts/nav/Nav.jsx
+++ frontend/src/layouts/nav/Nav.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as S from "./style";
 import { BiMessageRoundedDots } from "react-icons/bi";
 import { GoBell } from "react-icons/go";
 import { Link } from "react-router-dom"; 
 import useMediaQueries from "../../hooks/useMediaQueries";
+import { useAuthStore } from "../../store/useAuthStore";
 
 const Nav = () => {
   const {isDesktop } = useMediaQueries();
+  const { isLoggedIn, user, fetchUserInfo } = useAuthStore();
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      fetchUserInfo(); // 사용자 정보 요청
+    }
+  }, [isLoggedIn, fetchUserInfo]);
 
   return (
     <>
       <S.NavWrapper $isDesktop={isDesktop}>
-        <S.Text>이유진님, 환영합니다.</S.Text>
+        <S.Text>{user ? `${user.username}님, 환영합니다.` : '로그인 해주세요.'}</S.Text>
         <S.Divider />
         <S.StyledLink to="/mypage">
           <S.Text>마이페이지</S.Text>
