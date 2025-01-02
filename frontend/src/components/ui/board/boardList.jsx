686e91d23568e3137ab0c19040547ed2d1c804bc
wouldu <jhw664089@gmail.com>
2025-01-02 22:57:34 +0900

    feat #EM-88: 게시물 목록 API 연동동

--- frontend/src/components/ui/board/boardList.jsx
+++ frontend/src/components/ui/board/boardList.jsx
@@ -5,14 +5,19 @@ import Modal from "../modal/modalWrite";
 
 const BoardListWrite = ({ boardData }) => {
 
-  const BoardId = boardData.boardId || "2";
+  const boardId = boardData?.boardId || "2";
   const title = boardData?.title || "제목을 불러오는 중...";
   const content = boardData?.content || "내용을 불러오는 중...";
-  const author = boardData?.author?.username || "이유진";
+  const author = boardData?.author?.username || "작성자 없음";
   const createdAt = boardData?.createdAt || "2024.12.26";
-  const hit = boardData?.hit || "11";
-  const comments = boardData?.comments || "0";
-  const applicants = boardData?.applicants || { frontend: 0, backend: 0, uiux: 0, marketer: 0 };
+  const hit = boardData?.hit || "0";
+  const commentCount = boardData?.commentCount || "0";
+  const applyForms = boardData?.applyForms || { backend: 0, frontend: 0, marketer: 0, designer: 0 };
+  // 잘 넣어주셈 ㅎ.ㅎ
+  const isParticipated = boardData?.isParticipated || false;
+  const isClosed = boardData?.isClosed || false;
+  const username = boardData?.author.username || false;
+
 
   const [open, setOpen] = useState(false);
 
@@ -60,19 +65,19 @@ const BoardListWrite = ({ boardData }) => {
             <S.Icon>
               <S.CommentIcon />
             </S.Icon>
-            <div>{comments}</div>
+            <div>{commentCount}</div>
           </S.InfoItem>
           <S.ApplicantInfo>
             <div>지원자 수 |</div>
-            <div>프론트엔드 {applicants.frontend}명</div>
-            <div>백엔드 {applicants.backend}명</div>
-            <div>UI/UX {applicants.uiux}명</div>
-            <div>마케터 {applicants.marketer}명</div>
+            <div>프론트엔드 {applyForms.frontend}명</div>
+            <div>백엔드 {applyForms.backend}명</div>
+            <div>UI/UX {applyForms.uiux}명</div>
+            <div>마케터 {applyForms.marketer}명</div>
           </S.ApplicantInfo>
         </S.InfoContainer>
       </S.BoardListContainer>
     </S.LinkContainer>
-    <Modal isOpen={open} onClose={closeModal} boardId={BoardId}/>
+    <Modal isOpen={open} onClose={closeModal} boardId={boardId}/>
     </>
   );
 };
--- frontend/src/pages/home/list.jsx
+++ frontend/src/pages/home/list.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import BoardList from "../../components/ui/board/boardList";
 import { useLocation, useNavigate } from "react-router-dom";
+import useBoardListStore from "../../store/useBoardListStore";
 import * as S from "./style";  
 
 const List = () => {
-  const [boardData, setBoardData] = useState([]);
+  const { boards, fetchBoards, totalPages, currentPage, loading, error } = useBoardListStore();
   const itemsPerPage = 5;
 
   const navigate = useNavigate(); // 페이지 이동을 위한 useNavigate
@@ -14,26 +15,17 @@ const List = () => {
   const query = new URLSearchParams(location.search);
   const pageFromUrl = parseInt(query.get("page"), 10) || 1;
 
-  const [currentPage, setCurrentPage] = useState(pageFromUrl); // 페이지 상태 관리
-
-  const totalPages = Math.ceil(boardData.length / itemsPerPage); // 총 페이지 수 계산
-
-
-  // 현재 페이지 아이템 계산
-  const getCurrentPageItems = () => {
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    return boardData.slice(startIndex, startIndex + itemsPerPage);
-  };
-
-  const currentItems = getCurrentPageItems();
-
   // 페이지 변경 시 호출
   const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
     // URL에 페이지 번호 업데이트
     navigate(`?page=${pageNumber}`);
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
+
+  // URL에 맞춰 데이터 Fetch
+  useEffect(() => {
+    fetchBoards(pageFromUrl, itemsPerPage);
+  }, [fetchBoards, pageFromUrl]);
 
   // 페이지 번호 리스트 생성
   const pageNumbers = [];
@@ -41,172 +33,12 @@ const List = () => {
     pageNumbers.push(i);
   }
 
-  // URL에 맞춰 currentPage 상태 업데이트
-  useEffect(() => {
-    setCurrentPage(pageFromUrl);
-  }, [pageFromUrl]);
-
-  useEffect(() => {
-    // 예시 데이터
-    const exampleData = [
-      {
-        boardId: 1,
-        title: "프론트엔드 개발자 모집",
-        content: "React를 활용한 웹 애플리케이션 개발",
-        hit: 123,
-        comments: 4,
-        recruitmentType: "전체",
-        recruitmentCount: 3,
-        recruitmentField: "프론트엔드",
-        applicants: {
-          frontend: 1,
-          backend: 2,
-          uiux: 1,
-          marketer: 0,
-        },
-        recruitmentMethod: "원격",
-        recruitmentDeadline: "2024-12-31T23:59:59",
-        progressPeriod: "3개월",
-        createdAt: "11시간 전",
-        updatedAt: "2024-12-17T14:00:00",
-        author: {
-          userId: 101,
-          username: "John Doe",
-        },
-      },
-      {
-        boardId: 2,
-        title: "UI/UX 디자이너 모집",
-        content: "앱 인터페이스 디자인과 사용성 테스트",
-        hit: 85,
-        comments: 2,
-        recruitmentType: "프로젝트",
-        recruitmentCount: 2,
-        recruitmentField: "디자인",
-        applicants: {
-          frontend: 0,
-          backend: 0,
-          uiux: 2,
-          marketer: 1,
-        },
-        recruitmentMethod: "오프라인",
-        recruitmentDeadline: "2024-12-20T18:00:00",
-        progressPeriod: "6개월",
-        createdAt: "1시간 전",
-        updatedAt: null,
-        author: {
-          userId: 102,
-          username: "Jane Smith",
-        },
-      },
-      {
-        boardId: 3,
-        title: "UI/UX 디자이너 모집",
-        content: "앱 인터페이스 디자인과 사용성 테스트",
-        hit: 85,
-        comments: 2,
-        recruitmentType: "프로젝트",
-        recruitmentCount: 2,
-        recruitmentField: "디자인",
-        applicants: {
-          frontend: 0,
-          backend: 0,
-          uiux: 2,
-          marketer: 1,
-        },
-        recruitmentMethod: "오프라인",
-        recruitmentDeadline: "2024-12-20T18:00:00",
-        progressPeriod: "6개월",
-        createdAt: "1시간 전",
-        updatedAt: null,
-        author: {
-          userId: 102,
-          username: "Jane Smith",
-        },
-      },
-      {
-        boardId: 4,
-        title: "프론트엔드 개발자 모집",
-        content: "React를 활용한 웹 애플리케이션 개발",
-        hit: 123,
-        comments: 4,
-        recruitmentType: "스터디",
-        recruitmentCount: 3,
-        recruitmentField: "프론트엔드",
-        applicants: {
-          frontend: 1,
-          backend: 2,
-          uiux: 1,
-          marketer: 0,
-        },
-        recruitmentMethod: "원격",
-        recruitmentDeadline: "2024-12-31T23:59:59",
-        progressPeriod: "3개월",
-        createdAt: "11시간 전",
-        updatedAt: "2024-12-17T14:00:00",
-        author: {
-          userId: 101,
-          username: "John Doe",
-        },
-      },
-      {
-        boardId: 5,
-        title: "UI/UX 디자이너 모집",
-        content: "앱 인터페이스 디자인과 사용성 테스트",
-        hit: 85,
-        comments: 2,
-        recruitmentType: "프로젝트",
-        recruitmentCount: 2,
-        recruitmentField: "디자인",
-        applicants: {
-          frontend: 0,
-          backend: 0,
-          uiux: 2,
-          marketer: 1,
-        },
-        recruitmentMethod: "오프라인",
-        recruitmentDeadline: "2024-12-20T18:00:00",
-        progressPeriod: "6개월",
-        createdAt: "1시간 전",
-        updatedAt: null,
-        author: {
-          userId: 102,
-          username: "Jane Smith",
-        },
-      },
-      {
-        boardId: 6,
-        title: "UI/UX 디자이너 모집",
-        content: "앱 인터페이스 디자인과 사용성 테스트",
-        hit: 85,
-        comments: 2,
-        recruitmentType: "스터디",
-        recruitmentCount: 2,
-        recruitmentField: "디자인",
-        applicants: {
-          frontend: 0,
-          backend: 0,
-          uiux: 2,
-          marketer: 1,
-        },
-        recruitmentMethod: "오프라인",
-        recruitmentDeadline: "2024-12-20T18:00:00",
-        progressPeriod: "6개월",
-        createdAt: "1시간 전",
-        updatedAt: null,
-        author: {
-          userId: 102,
-          username: "Jane Smith",
-        },
-      },
-    ];
-
-    setBoardData(exampleData);
-  },  []);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
 
   return (
     <>
-       {currentItems.map((board) => ( 
+       {boards.map((board) => ( 
             <BoardList key={board.boardId} boardData={board} />
         ))}
       {totalPages > 1 && (
