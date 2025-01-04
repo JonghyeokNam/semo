0922364592d638e144c439881e8b5aa2685d06fb
wouldu <jhw664089@gmail.com>
2025-01-04 20:17:31 +0900

    fix #EM-88: 마이페이지 지원서 확인 시 에러 해결

--- frontend/src/components/ui/board/boardListApplyList.jsx
+++ frontend/src/components/ui/board/boardListApplyList.jsx
@@ -11,7 +11,7 @@ const BoardListApplyList = ({ boardData }) => {
   const createdAt = boardData?.createdAt || "2024.12.26";
   const hit = boardData?.hit || "11";
   const comments = boardData?.comments || "0";
-  const applicants = boardData?.applicants || { frontend: 0, backend: 0, uiux: 0, marketer: 0 };
+  const applyForms = boardData?.applyForms || { frontend: 0, backend: 0, uiux: 0, marketer: 0 };
 
   return (
     <S.LinkContainer100 to="/board/detail" state={{ boardData }}>
@@ -49,10 +49,10 @@ const BoardListApplyList = ({ boardData }) => {
           </S.InfoItem>
           <S.ApplicantInfo>
             <div>지원자 수 |</div>
-            <div>프론트엔드 {applicants.frontend}명</div>
-            <div>백엔드 {applicants.backend}명</div>
-            <div>UI/UX {applicants.uiux}명</div>
-            <div>마케터 {applicants.marketer}명</div>
+            <div>프론트엔드 {applyForms.frontend}명</div>
+            <div>백엔드 {applyForms.backend}명</div>
+            <div>UI/UX {applyForms.uiux}명</div>
+            <div>마케터 {applyForms.marketer}명</div>
           </S.ApplicantInfo>
         </S.InfoContainer>
       </S.BoardListContainer>
